f313da7f2cad7ca83efe88257b416b4d1972a512
kumilachew-g <kumilachew1212@gmail.com>
2022-09-13 15:42:11 +0300

    Add Particles and Configured its dependencies

---
+++ src/components/Particles.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+
+const ParticlesComponent = () => (
+  <Particles
+    id='particles-js'
+    params={{
+      particles: {
+        number: {
+          value: 400,
+          density: {
+            enable: true,
+            value_area: 1000,
+          },
+        },
+        color: {
+          value: '#fff',
+        },
+        opacity: {
+          value: 0.5,
+          anim: {
+            enable: true,
+          },
+        },
+        size: {
+          value: 7,
+          random: true,
+          anim: {
+            enable: true,
+            speed: 4,
+          },
+        },
+        line_linked: {
+          enable: false,
+        },
+        move: {
+          speed: 0.2,
+        },
+      },
+    }}
+  />
+);
+
+export default ParticlesComponent;
