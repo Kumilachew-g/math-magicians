bf505c4e204eb24c2ee0da12507a63de91cf7dcc
kumilachew-g <kumilachew1212@gmail.com>
2022-09-13 16:29:46 +0300

    Add Navbar Components and Routers

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React from 'react';
 import './App.css';
 import Calculator from './components/calculator';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import NotMatch from './pages/NotMatch';
+import Navbar from './components/Navbar';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      digitObj: {
-        totalOutput: 0,
-        nextOutput: 0,
-        operation: null,
-      },
-    };
-    this.updateState = this.updateState.bind(this);
-  }
-
-  updateState(obj) {
-    this.setState({
-      digitObj: obj,
-    });
-  }
-
-  render() {
-    const { digitObj } = this.state;
-    return (
-      <div className="App">
-        <Calculator updateState={this.updateState} digitObj={digitObj} />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Navbar />
+    <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/calculator'>
+        <Calculator />
+      </Route>
+      <Route path='/quote'>
+        <Quote />
+      </Route>
+      <Route path='*'>
+        <NotMatch />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
