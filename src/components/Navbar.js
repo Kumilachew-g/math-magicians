255e62691d244f815140a0bc59eda1e91c3f3a61
kumilachew-g <kumilachew1212@gmail.com>
2022-09-13 15:41:05 +0300

    Add NavLink Page Using Arrays

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Links = [
+  {
+    id: 1,
+    name: 'Home',
+    path: '/',
+  },
+  {
+    id: 2,
+    name: 'Calculator',
+    path: '/calculator',
+  },
+  {
+    id: 3,
+    name: 'Qoute',
+    path: '/quote',
+  },
+];
+
+const Navbar = () => (
+  <nav className='nav'>
+    <h1>Math Magician</h1>
+    <ul>
+      {Links.map((link) => (
+        <NavLink to={link.path} key={link.id} className='links'>
+          {link.name}
+        </NavLink>
+      ))}
+    </ul>
+  </nav>
+);
+
+export default Navbar;
