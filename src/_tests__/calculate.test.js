1bbb4e00f92382fb7e8b7d71baf2b4703323ec5d
kumilachew-g <kumilachew1212@gmail.com>
2022-09-14 18:50:42 +0300

    Add Calculator Test Functions

---
+++ src/_tests__/calculate.test.js
@@ -0,0 +1,57 @@
+import calculate from '../logic/calculate';
+
+describe('calculate function tests ', () => {
+  test('AC button with no  input', () => {
+    const cal = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    const result = calculate(cal, 'AC');
+    expect(result.total).toBe(null);
+    expect(result.next).toBe(null);
+    expect(result.operation).toBe(null);
+  });
+
+  test('AC with input', () => {
+    const cal = {
+      total: 10,
+      next: 2,
+      operation: '-',
+    };
+    const result = calculate(cal, 'AC');
+    expect(result.total).toBe(null);
+    expect(result.next).toBe(null);
+    expect(result.operation).toBe(null);
+  });
+  test('+/- with no input', () => {
+    const cal = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+
+    const result = calculate(cal, '+/-');
+    expect(result).toMatchObject({});
+  });
+  test('+/- with +ve input', () => {
+    const cal = {
+      total: 10,
+      next: null,
+      operation: null,
+    };
+
+    const result = calculate(cal, '+/-');
+    expect(result.total).toBe('-10');
+  });
+  test('+/- with  -ve input', () => {
+    const cal = {
+      total: -10,
+      next: null,
+      operation: null,
+    };
+
+    const result = calculate(cal, '+/-');
+    expect(result.total).toBe('10');
+  });
+});
