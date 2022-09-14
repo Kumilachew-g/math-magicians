a232d4e755366dd0c78dc13a8040140033137042
kumilachew-g <kumilachew1212@gmail.com>
2022-09-14 21:21:49 +0300

    Add Home Page Test Snapshot

--- src/_tests__/Home.test.js
+++ src/_tests__/Home.test.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import Home from '../pages/Home';
 
 it('expect homepage to match working snapshot', () => {
-  const homePage = renderer
-    .create(
-      <MemoryRouter>
-        <Home />
-      </MemoryRouter>,
-    )
-    .toJSON();
+  const homePage = renderer.create(<Home />).toJSON();
   expect(homePage).toMatchSnapshot();
 });
