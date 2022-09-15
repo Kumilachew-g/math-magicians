531ab16a1dac8836c7a16899c56db0fba0527ed1
kumilachew-g <kumilachew1212@gmail.com>
2022-09-15 11:36:30 +0300

    Check Component Snapshot Test

--- src/_tests__/Particles.test.js
+++ src/_tests__/Particles.test.js
@@ -1,5 +1,6 @@
 import Particles from 'react-particles-js';
 import { render } from '@testing-library/react';
+import 'jest-canvas-mock';
 
 test('expect Particle page to match working snapshot', () => {
   const particlePage = render(<Particles />);
