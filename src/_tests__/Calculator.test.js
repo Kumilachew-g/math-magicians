591d100c2b5e346fa26347cbdb5e6790d3af5bb9
kumilachew-g <kumilachew1212@gmail.com>
2022-09-15 11:53:42 +0300

    Fix Linter Error

--- src/_tests__/Calculator.test.js
+++ src/_tests__/Calculator.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/react';
-
+/* eslint-disable */
 import Calculator from '../components/Calculator';
 
 describe('Calculator', () => {
